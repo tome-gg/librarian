f6c12c8f40dd718761b02d19d373fe3ab96cc9b6
Darren Sapalo <darren.sapalo@gmail.com>
2023-05-13 17:18:52 +0800

    Added install scripts for Tome and GH to make installing the CLI tool simpler.

---
+++ install/install.ps1
@@ -0,0 +1,22 @@
+# PowerShell
+
+# Define the URL for the Windows binary
+$WINDOWS_BINARY_URL="https://github.com/tome-gg/librarian/releases/download/0.1.0/tome-win.exe"
+
+# Check if gh CLI tool is installed
+if (!(Get-Command gh -ErrorAction SilentlyContinue)) {
+    Write-Output "gh CLI tool is not installed. Installing now..."
+
+    # Download and install the GitHub CLI
+    Invoke-WebRequest -Uri "https://github.com/cli/cli/releases/download/v2.0.0/gh_2.0.0_windows_amd64.msi" -OutFile gh.msi
+    Start-Process -Wait -FilePath msiexec -ArgumentList /i, (Resolve-Path .\gh.msi)
+    Remove-Item .\gh.msi
+}
+
+# Download the binary
+Invoke-WebRequest -Uri $WINDOWS_BINARY_URL -OutFile tome.exe
+
+# Move the binary to a directory in the PATH
+# Here we use C:\Windows\System32, which is generally in the PATH, but you should 
+# adjust this to your preferred location.
+Move-Item -Path .\tome.exe -Destination C:\Windows\System32\tome.exe
---
+++ install/install.zsh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+# Define the URLs for the binaries
+MAC_BINARY_URL="https://github.com/tome-gg/librarian/releases/download/0.1.0/tome-darwin-arm-osx-m1"
+LINUX_BINARY_URL="https://github.com/tome-gg/librarian/releases/download/0.1.0/tome-linux-amd64"
+
+# Check if gh CLI tool is installed
+if ! command -v gh &> /dev/null; then
+    echo "gh CLI tool is not installed. Installing now..."
+
+    # Detect the operating system
+    OS="$(uname)"
+
+    # Install gh CLI tool
+    if [ "$OS" == "Darwin" ]; then
+        # macOS
+        brew install gh
+    elif [ "$OS" == "Linux" ]; then
+        # Linux
+        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+        sudo apt-add-repository https://cli.github.com/packages
+        sudo apt update
+        sudo apt install gh
+    else
+        echo "Unsupported operating system: $OS"
+        exit 1
+    fi
+fi
+
+# Detect the operating system
+OS="$(uname)"
+
+# Download and configure the appropriate binary
+if [ "$OS" == "Darwin" ]; then
+    # macOS
+    curl -L $MAC_BINARY_URL -o tome
+elif [ "$OS" == "Linux" ]; then
+    # Linux
+    curl -L $LINUX_BINARY_URL -o tome
+else
+    echo "Unsupported operating system: $OS"
+    exit 1
+fi
+
+# Make the binary executable
+chmod +x tome
+
+# Move the binary to a directory in the PATH
+sudo mv tome /usr/local/bin/
